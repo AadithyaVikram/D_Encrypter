856d0c9d503946cde1208556883c49c58163cf50
AadithyaVikram <86481204+AadithyaVikram@users.noreply.github.com>
2024-07-28 01:49:29 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,85 @@
+from tkinter import *
+import random as FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTYfGwVx
+encrypt=Tk()
+encrypt.title("D-'EN'-crypter")
+
+def a(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx):
+    if len(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx)==0:
+        v="give input"
+        return v
+    else:
+        if len(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx) % 2 == 0:
+            pass
+        else:
+            FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx + " "
+        FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGVx = FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTYfGwVx.randrange(129, 200)
+        FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = "ǆËǤĄɄŕƫŲ" + FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx[:(len(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx) // 2)] + "ŤȑäĴ£ƿƮɂ" + FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx[(len(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx) // 2):] + "ÍôōÝĭǶƛģ"
+        FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = ""
+        for FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItARxVyNoQbKpCnLpAxIxTeYfGwVx in FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx:
+            FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = ord(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItARxVyNoQbKpCnLpAxIxTeYfGwVx)
+            FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx + FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGVx
+            FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSlDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = chr(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx)
+            FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx + FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSlDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx
+
+        FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGVx = FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGVx + 483
+        FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx = FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx + str(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDzJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGVx)
+        return(FuAeFtSqLzGfSwIsTyPoZgAmFuAeFtSqLzGfSwIsTyPoZgAmSiDJfQhNdLzFyOsXzUuSiYyQhBzQzZjRsAcAlWuZiItAhRxVyNoQbKpCnLpAxIxTeYfGwVx)
+def b(qwer):
+    if len(qwer)==0:
+        u="give the input"
+        return u
+    elif len(qwer)>=0:
+        qwer=str(qwer)
+        mn = int(qwer[len(qwer) - 4:len(qwer)])
+        mn = mn - 483
+        l123 = []
+        for u1 in qwer:
+            l123.append(u1)
+        for s123 in range(4):
+            l123.pop(len(qwer) - s123 - 1)
+        d123 = ""
+        for h123 in l123:
+            d123 = d123 + h123
+        w123 = ""
+        for i in d123:
+            y = ord(i)
+            y = y - mn
+            q = chr(y)
+            w123 = w123 + q
+        l1 = []
+        for e123 in w123:
+            l1.append(e123)
+        for q1 in range(8):
+            l1.pop(len(w123) - 1 - q1)
+        l5 = l1[8:]
+        t123 = len(l5) // 2
+        l11 = l5[:t123 - 4]
+        l12 = l5[t123 + 4:]
+        asdfjkl = ""
+        for qwe in l11:
+            asdfjkl = asdfjkl + qwe
+        for asdf in l12:
+            asdfjkl = asdfjkl + asdf
+        return asdfjkl
+
+def erun():
+    code=writer.get('1.0',END)
+    output.delete('1.0',END)
+    output.insert('1.0', a(code))
+def drun():
+    infor=writer.get('1.0',END)
+    output.delete('1.0',END)
+    output.insert('1.0',b(infor))
+menu_bar=Menu(encrypt)
+run_bar=Menu(menu_bar,tearoff=0)
+run_bar.add_command(label='encrypter',command=erun)
+run_bar.add_command(label='decrypter',command=drun)
+menu_bar.add_cascade(label='Run',menu=run_bar)
+encrypt.config(menu=menu_bar)
+
+writer=Text()
+writer.pack()
+output=Text()
+output.pack()
+encrypt.mainloop()
+
